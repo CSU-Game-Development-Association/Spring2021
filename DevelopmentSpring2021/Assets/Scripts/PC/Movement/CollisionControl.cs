322b9122754888404743e59c517ad944bedf95ad
Waterbot04 <65328143+Waterbot04@users.noreply.github.com>
2021-01-20 20:28:44 -0700

    Test

---
+++ DevelopmentSpring2021/Assets/Scripts/PC/Movement/CollisionControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionControl : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        print("Landed on " + collision.gameObject.name);
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        print("Liftoff");
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        print("Walking");
+    }
+}
