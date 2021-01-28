f69bbe44e1438aaac499efdd6dbe09c0711d55f7
lifelandman <jacksonjams3@gmail.com>
2021-01-27 19:06:58 -0700

    Test enviroment and Visual example

---
+++ DevelopmentSpring2021/Assets/code/onClick.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class onClick : MonoBehaviour
+{
+
+    RaycastHit hit;
+    public Vector3 attentionPoint;
+    
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+            {
+
+                attentionPoint = hit.point;
+                transform.position = attentionPoint;
+            }
+        }
+        
+    }
+}
---
+++ DevelopmentSpring2021/Assets/code/walkingTest.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class walkingTest : MonoBehaviour
+{
+    [SerializeField]
+    Transform _destination;
+
+    UnityEngine.AI.NavMeshAgent _navMeshAgent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _navMeshAgent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
+
+        //I'm following the tutorial closely for now, but even if anything else is changed, we should be sure to use debug messages.
+        if(_navMeshAgent == null)
+        {
+            Debug.LogError("NavMeshAgent is missing!" + gameObject.name);
+        }
+    }
+
+
+    private void LandHo()
+    {
+
+        if(_destination != null)
+        {
+            Vector3 targetvector = _destination.transform.position;
+            _navMeshAgent.SetDestination(targetvector);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            LandHo();
+        }
+    }
+
+}
