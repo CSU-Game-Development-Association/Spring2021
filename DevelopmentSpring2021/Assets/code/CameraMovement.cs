78dcaa9c34337ad8756b49aa178ea20b49608b55
lifelandman <jacksonjams3@gmail.com>
2021-02-14 19:42:44 -0700

    Camera System
    Added an axis based camera system
    
    Bugs:
    On occasion, moving the camera while the character will move will case shaking.

---
+++ DevelopmentSpring2021/Assets/code/CameraMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField]
+    Transform activeChar;
+    Vector3 cameraDeform;
+    
+    
+    
+    void Start()
+    {
+        
+        
+    }
+
+
+    void Update() //I want to add more and better features to the camera later eg. if the player can select different characters to control from bird's eye
+    {
+        
+    }
+
+    void LateUpdate()
+    {
+        cameraDeform = transform.forward;
+
+        if(Input.GetMouseButton(0))
+        {
+            transform.RotateAround(activeChar.position, transform.right, Input.GetAxis("Mouse Y") * 2);
+            transform.RotateAround(activeChar.position, activeChar.up, Input.GetAxis("Mouse X") * 2);
+
+        }
+        if(activeChar.hasChanged)
+        {
+            transform.position = activeChar.position - (cameraDeform * 10);
+            activeChar.hasChanged = false;
+        }
+    }
+}
